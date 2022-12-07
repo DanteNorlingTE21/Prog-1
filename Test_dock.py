13f3f9bb333dd29b4e4a0d7c9009aacb814e34c1
Dante Norling <dante.norling@elev.ga.ntig.se>
2022-12-07 13:11:44 +0100

    k

--- Test_dock.py
+++ Test_dock.py
@@ -1,10 +1,5 @@
-import time
-
-
-now = time.time()
-time_delta = 0
-while True:
-
-    time_delta = time.time() - now
-    now = time.time()
-    print(time_delta)
+for i in range(3):
+    print(i)
+    break
+else:
+    print(3)
